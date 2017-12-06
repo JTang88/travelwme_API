3587edb33da285583ff021c7f7dba3a46311f63b
Jerry Tang <jerrytang@Jerrys-MacBook-Pro.local>
2017-12-05 17:39:18 -0800

    created prouction.js file in rest-server/bin for complle for production and wrote npm start npm clean npm build and npm production script for rest server

--- rest-server/src/index.js
+++ rest-server/src/index.js
@@ -5,7 +5,9 @@ const app = express()
 
 let port = 3001 || process.env.PORT;
 
+console.log("if you see what this log you will know if .env is still working ======", process.env.TOKEN_SECRET)
+
 app.get('/', (req, res) => res.send('Hello World!'))
 app.use('/api', router);
 
-app.listen(port, () => console.log(`rest-server listening on port ${port}!`))//
+app.listen(port, () => console.log(`rest-server listening on port ${port}!`));
