cc6ee10136767dd98da9c4be8b3b329d4a837395
Jerry Tang <jerrytang@Jerrys-MacBook-Pro.local>
2017-12-05 18:56:54 -0800

    added production.js file to socket-server/bin and wrote script in package.json, npm start npm run prodcution npm run clean and npm run build

--- socket-server/bin/dev.js
+++ socket-server/bin/dev.js
@@ -2,9 +2,11 @@ require('babel-register');
 require('babel-polyfill');
 
 const env = require('dotenv');
+const path = require('path');
+
 //configure dotenv here
 env.config({
-  path: '../.env'
+  path: path.join(__dirname, '/../.env')
 })
 
 require('../src')
