d70022029c1ca6abf99cb2141d396735745edfb4
Jerry Tang <jerrytang@Jerrys-MacBook-Pro.local>
2017-12-08 01:10:30 -0800

    recommit - implement test logic for graphQL but realize current folder with mutiple package.json files structure has issue and decided to switch to a new branch called folder_Structure_reset

--- rest-server/src/app.js
+++ rest-server/src/app.js
@@ -1,9 +1,27 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import { graphiqlExpress, graphqlExpress } from 'apollo-server-express';
+import { makeExecutableSchema } from 'graphql-tools';
 import router from './routes';
+import typeDefs from './schema';
+import resolvers from './resolvers';
 
 const app = express();
 
+export const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers,
+});
+
+app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
+app.use('/graphiql', graphiqlExpress({
+  endpointURL: '/graphql',
+}));
+
 app.use('/api', router);
-app.get('/', (req, res) => res.send('Hello World!'));
+
+
+// app.get('/', (req, res) => res.send('Hello World!'));
+
 
 export default app;
