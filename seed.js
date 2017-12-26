ca2b6ca5ff061d4eb1a85caaf2ba05abf72db83f
Jerry Tang <jerrytang@Jerrys-MacBook-Pro.local>
2017-12-25 19:09:54 -0800

    modify seed.js and newdata seed data for body_type and tripKeyworks table

--- seed.js
+++ seed.js
@@ -3,65 +3,76 @@ import seedData from './newdata.json';
 // const db = require ('./db');
 // const seedData = require('./newdata.json');
 
+// db.TripKeyword.bulkCreate(seedData.TripKeyword)
+// .then(() => {
+//  console.log('created trip keyword');
+// })
+// .catch((err) => {
+//  console.log('error', err);
+// });
+
+db.BodyType.bulkCreate(seedData.BodyType)
+.then(() => {
+ console.log('created bodytype');
+})
+.catch((err) => {
+ console.log('error', err);
+});
+
+// db.TripKeyword.update({ word: 'Backpacker'}, { where: { id: 2 } });
+
+// db.TripKeyword.destroy({ where: { id: 8 } });
+
 db.TripKeyword.bulkCreate(seedData.TripKeyword)
 .then(() => {
   console.log('created trip keyword');
 })
-.catch((err) => {
-  console.log('error', err);
-});
+// .catch((err) => {
+//   console.log('error', err);
+// });
 
-db.BodyType.bulkCreate(seedData.BodyType)
-.then(() => {
-  console.log('created bodytype');
-})
-.catch((err) => {
-  console.log('error', err);
-});
-
-db.User.bulkCreate(seedData.User)
-  .then(() => {
-    console.log('created user');
-  })
-  .catch((err) => {
-    console.log('error', err);
-  });
-
-db.Trip.bulkCreate(seedData.Trip)
-  .then((trips) => {
-    trips.forEach((trip, idx) => {
-      trip.addTripKeywords(seedData.Trip[idx].keys);
-      trip.addBodyType(seedData.Trip[idx].body_types);
-      
-    });
-    // console.log('my test array', test[0].addTripKeywords);
-  })
-  .then(() => {
-    console.log('created trip');
-  })
-  .catch((err) => {
-    console.log('error', err);
-  });
-
-
-db.TripMembers.bulkCreate(seedData.TripMembers)
-  .then(() => {
-    console.log('created tripmembers');
-  })
-  .catch((err) => {
-    console.log('error', err);
-  });
-
-// db.Users.sync({ force: true }).then(() => {
-//   data.Users.forEach((us) => {
-//     db.Users.create({
-//       username: us.username,
-//       email: us.email,
-//       password: us.password,
-//       age: us.age,
-//       fitness: us.fitness,
-//       gender: us.gender,
-//       relationship_status: us.relationship_status,
-//     });
-//   });
+// db.BodyType.bulkCreate(seedData.BodyType)
+// .then(() => {
+//   console.log('created bodytype');
 // })
+// .catch((err) => {
+//   console.log('error', err);
+// });
+
+// db.User.bulkCreate(seedData.User)
+//   .then(() => {
+//     console.log('created user');
+//   })
+//   .catch((err) => {
+//     console.log('error', err);
+//   });
+
+// db.Trip.bulkCreate(seedData.Trip)
+//   .then((trips) => {
+//     trips.forEach((trip, idx) => {
+//       trip.addTripKeywords(seedData.Trip[idx].keys);
+//       trip.addBodyType(seedData.Trip[idx].body_types);
+      
+//     });
+//     // console.log('my test array', test[0].addTripKeywords);
+//   })
+//   .then(() => {
+//     console.log('created trip');
+//   })
+//   .catch((err) => {
+//     console.log('error', err);
+//   });
+
+
+// db.TripMembers.bulkCreate(seedData.TripMembers)
+//   .then(() => {
+//     console.log('created tripmembers');
+//   })
+//   .catch((err) => {
+//     console.log('error', err);
+//   });
+
+
+
+
+
