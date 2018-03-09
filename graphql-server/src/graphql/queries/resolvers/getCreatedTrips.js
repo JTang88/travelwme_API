996b48f6a1ac153920a8f3de59bb7ed2490e757c
Jerry Tang <jerrytang@Jerrys-MacBook-Pro.local>
2018-03-09 13:40:11 -0700

    [Fix]:getCreatedTrip- fix query to fit new creatorId creator logic

--- graphql-server/src/graphql/queries/resolvers/getCreatedTrips.js
+++ graphql-server/src/graphql/queries/resolvers/getCreatedTrips.js
@@ -1,21 +1,9 @@
 export default {
   getCreatedTrips: async (parent, { id }, { models }) => {
-    const targetedTripIds = [];
-    const targetedTrips = await models.TripMembers.findAll({
-      where: {
-        userId: id,
-        user_type: 'C',
-      }
-    });
-
-    for(let i = 0; i < targetedTrips.length; i++) {
-      targetedTripIds.push(targetedTrips[i].dataValues.tripId);
-    }
-
     return models.Trip.findAll({
-       where: {
-         id: targetedTripIds,
-       }
-    })
-  },
-};
+      where: {
+        creatorId: id,
+      },
+    });
+  }
+}
